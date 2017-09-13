cfda8f5454b7ea6f07147f23197db05bc2443cb5
huji <huji0624@gmail.com>
2017-09-14 00:12:30 +0800

    first ml player.

--- main.py
+++ main.py
@@ -11,13 +11,18 @@ ge = gameengine.GameEngine(GameConfig(round_limit=200000,save_limit=10000, model
                                       chips_model=GAME_CHIPS_MODEL_CLEAR))
 
 ge.addPlayer(StatisticsTightPlayer("s1"))
+ge.addPlayer(StatisticsTightPlayer("s2"))
+ge.addPlayer(StatisticsTightPlayer("s3"))
+ge.addPlayer(StatisticsTightPlayer("s4"))
+ge.addPlayer(StatisticsTightPlayer("s5"))
+ge.addPlayer(StatisticsTightPlayer("s6"))
 
 # ge.addPlayer(LittleTightPlayer("t1"))
-ge.addPlayer(LittleTightPlayer("t2"))
-ge.addPlayer(LittleTightPlayer("t3"))
-ge.addPlayer(LittleTightPlayer("t4"))
-ge.addPlayer(LittleTightPlayer("t5"))
-ge.addPlayer(LittleTightPlayer("t6"))
+# ge.addPlayer(LittleTightPlayer("t2"))
+# ge.addPlayer(LittleTightPlayer("t3"))
+# ge.addPlayer(LittleTightPlayer("t4"))
+# ge.addPlayer(LittleTightPlayer("t5"))
+# ge.addPlayer(LittleTightPlayer("t6"))
 
 # ge.addPlayer(randomPlayer("r1"))
 # ge.addPlayer(randomPlayer("r2"))
